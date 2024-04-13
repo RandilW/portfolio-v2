ad8852aa94a660c9000fc75b39f01c848520fe42
RandilW <85533296+RandilW@users.noreply.github.com>
2024-04-13 05:54:00 -0500

    updated connect

--- app/page.jsx
+++ app/page.jsx
@@ -71,7 +71,7 @@ const Home = () => {
         <ProjectsSection ref={ProjectsSectionRef} />
         <MyStack />
         <Experience />
-        <Testimonials />
+        {/*<Testimonials />*/}
       </main>
     </>
   );
