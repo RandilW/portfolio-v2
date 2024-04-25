439bb60139c438444963ba57e207073dfefe7812
RandilW <85533296+RandilW@users.noreply.github.com>
2024-04-25 01:15:13 -0500

    updated projects

--- app/components/json/techStack.js
+++ app/components/json/techStack.js
@@ -26,9 +26,9 @@ export const techStack = {
     },
     { name: "Git", src: "/techStack/git.svg", alt: "git_pic" },
     {
-      name: "Chakra UI",
-      src: "/techStack/chakraUI.svg",
-      alt: "chakra_ui_pic",
+      name: "Java",
+      src: "/techStack/java.svg",
+      alt: "java_pic",
     },
   ],
 
