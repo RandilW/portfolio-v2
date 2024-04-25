936dd96f700e4dcc9320fadf046c9329a6685d27
RandilW <85533296+RandilW@users.noreply.github.com>
2024-04-25 01:02:36 -0500

    updated about pic filter

--- app/about/components/ImageSlide.jsx
+++ app/about/components/ImageSlide.jsx
@@ -18,7 +18,7 @@ const ImageSlide = ({ src }) => {
       <Image
         src={profilePic}
         alt="Randil Wijayananda"
-        style={{ opacity: isInView ? 1 : 0 }}
+        style={{ opacity: isInView ? 1 : 0 , filter: 'grayscale(100%)'}}
         fill
         sizes="(max-width: 374px) 235px, (min-width: 768px) 335px, (min-width: 768px) 992px, (min-width: 1920px) 435px"
         // placeholder="blur"
