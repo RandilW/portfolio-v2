59f3d05ff259e3d97f9f9acc1b7b77044b7b760d
RandilW <85533296+RandilW@users.noreply.github.com>
2024-06-14 08:50:53 -0500

    updated profile pic

--- app/about/components/ImageSlide.jsx
+++ app/about/components/ImageSlide.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import styles from "../styles/slideimage.module.css";
-import profilePic from "../../../public/profile_pic.png";
+import profilePic from "../../../public/profile-pic(1).png";
 
 const ImageSlide = ({ src }) => {
   const [isInView, setIsInView] = useState(false);
@@ -18,7 +18,8 @@ const ImageSlide = ({ src }) => {
       <Image
         src={profilePic}
         alt="Randil Wijayananda"
-        style={{ opacity: isInView ? 1 : 0 , filter: 'grayscale(100%)'}}
+        // style={{ opacity: isInView ? 1 : 0 , filter: 'grayscale(100%)'}}
+        style={{ opacity: isInView ? 1 : 0 , pointerEvents: 'none'}}
         fill
         sizes="(max-width: 374px) 235px, (min-width: 768px) 335px, (min-width: 768px) 992px, (min-width: 1920px) 435px"
         // placeholder="blur"
